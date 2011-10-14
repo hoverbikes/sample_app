0c26831458d762767d36303b70d303412b63fe72
Jon Strand <jon.g.strand@gmail.com>
2011-10-14 10:35:57 -0500

    micropost validations

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,5 +1,51 @@
 require 'spec_helper'
 
 describe Micropost do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  before(:each) do
+    @user = Factory(:user)
+    @attr = {:content => "blah blah blah"}
+  end
+
+  it "should create a new instance with valid attributes" do
+    @user.microposts.create!(@attr)
+  end
+
+  describe "user associations" do
+    before(:each) do
+      @micropost = @user.microposts.create(@attr)
+    end
+    it "should have a user attribute" do
+      @micropost.should respond_to(:user)
+    end
+    it "should have the right associated user" do
+      @micropost.user_id.should == @user.id
+      @micropost.user.should == @user
+    end
+  end #user associations
+  
+  describe "validations" do
+    it "should have a user id" do
+      Micropost.new(@attr).should_not be_valid
+    end
+    it "should require nonblank content" do
+      @user.microposts.build(:content => "    ").should_not be_valid      
+    end
+    it "should reject overlong content" do
+      @user.microposts.build(:content => "x" * 141).should_not be_valid
+    end
+  end
+  
+  
+end #Micropost
+
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :integer         not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
