88d038ab544fdbec27487811f48bad602877c5fa
Jon Strand <jon.g.strand@gmail.com>
2011-10-13 19:39:58 -0500

    User Index dummy data and pagination

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 Factory.define :user do |user|
   
-  user.sequence (:name)         { |n| "Michael Hartl #{n} "}
+  user.sequence (:name)         { |n| "Michael Hartl #{n}"}
   user.sequence (:email)        { |n| "mhartl#{n}@example.com" }
   user.password                 "foobar"
   user.password_confirmation    "foobar"
